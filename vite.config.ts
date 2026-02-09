1c3b5a0a0c85d9b8c12933b75e6d6d47948ab593
fka-kafka <kigenbrandon@gmail.com>
2026-02-09 22:00:26 +0300

    fix(config): fixed root path that hindered asset retrieval

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  base: "/brandon-kigen/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
